31c356238037c473b7c5df8b8cc1fc836c07f9fe
Miki Blasco <blascom@7@gmail.com>
2022-06-06 18:32:19 +0200

    lab done

--- lab402/src/main/java/com/example/lab402/controller/PatientController.java
+++ lab402/src/main/java/com/example/lab402/controller/PatientController.java
@@ -1,13 +1,16 @@
 package com.example.lab402.controller;
 
+import com.example.lab402.Enums.Status;
 import com.example.lab402.model.Employee;
 import com.example.lab402.model.Patient;
+import com.example.lab402.repository.EmployeeRepository;
 import com.example.lab402.repository.PatientRepository;
 import com.sun.istack.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -16,6 +19,8 @@ public class PatientController {
 
     @Autowired
     PatientRepository patientRepository;
+    @Autowired
+    EmployeeRepository employeeRepository;
 
     @GetMapping("/all_Patients")
     public List<Patient> getAllPatients(){
@@ -37,6 +42,26 @@ public class PatientController {
     @ResponseStatus(HttpStatus.OK)
     public List<Patient> findByRangeOfBirthDate(@RequestParam @NotNull Date min, @RequestParam @NotNull Date max){
         return patientRepository.findByBirthBetween(min, max);
+    }
+
+    @GetMapping("/OFFDoctors-patients")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Patient> findPatientsByOffStatusDoctors(){
+        ;
+        List newList = new ArrayList<>();
+        for (Patient p : patientRepository.findAll()){
+            if (p.getAdmitted_by().getStatus().equals(Status.OFF)) {
+                newList.add(p);
+            }
+        }
+        return newList;
+    }
+
+
+    @GetMapping("/OFFDoctors-patients2")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Patient> findPatientsByOffStatusDoctors2(){
+        return patientRepository.findPatientswithDoctorOff();
     }
 
 }
--- lab402/src/main/java/com/example/lab402/repository/PatientRepository.java
+++ lab402/src/main/java/com/example/lab402/repository/PatientRepository.java
@@ -4,6 +4,7 @@ import com.example.lab402.Enums.Status;
 import com.example.lab402.model.Employee;
 import com.example.lab402.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Date;
@@ -16,6 +17,10 @@ public interface PatientRepository extends JpaRepository<Patient, Integer> {
     Optional<Patient> findById(Integer id);
 
     List<Patient> findByBirthBetween(Date min, Date max);
+
+    @Query(value = "SELECT * FROM patient JOIN employee ON patient.admitted_by = employee.id WHERE status = 'OFF'", nativeQuery = true)
+    public List<Patient> findPatientswithDoctorOff();
+
 
 
 }
